cfe45e77a6643a11d7e4e7fa142c2d635040586b
PeterFlandin <peterflandin@gmail.com>
2023-06-16 14:47:02 +0200

    conteneur leger

--- src/main/java/com/mycompany/dvd/App.java
+++ src/main/java/com/mycompany/dvd/App.java
@@ -1,28 +1,20 @@
 package com.mycompany.dvd;
 
 import com.mycompany.dvd.controller.MovieController;
-import com.mycompany.dvd.repository.FileMovieRepository;
-import com.mycompany.dvd.service.DefaultMovieService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
             
     {
 
-        MovieController movieController = new MovieController();
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MovieController movieController =  context.getBean(MovieController.class);
 
-        FileMovieRepository goLiveMovieRepository = new FileMovieRepository();
-        DefaultMovieService movieService = new DefaultMovieService();
-movieController.setMovieServiceInterface(movieService);
-movieService.setMovieRepositoryInterface(goLiveMovieRepository);
 
         movieController.addUsingControle();
-
 
 
     }
