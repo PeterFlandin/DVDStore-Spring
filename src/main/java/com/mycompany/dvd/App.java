253b2370e5e2a8b683933bcada9a6bff95006a54
PeterFlandin <peterflandin@gmail.com>
2023-06-19 16:31:36 +0200

    app devien config et on supprime le AppConfig.java

--- src/main/java/com/mycompany/dvd/App.java
+++ src/main/java/com/mycompany/dvd/App.java
@@ -2,16 +2,19 @@ package com.mycompany.dvd;
 
 import com.mycompany.dvd.controller.MovieController;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.*;
 
+@Configuration
+@ComponentScan(basePackages = {"com.mycompany.dvd.controller", "com.mycompany.dvd.service", "com.mycompany.dvd.repository.file"})
+@ImportResource("classpath:applicationContext.xml")
+@PropertySource("classpath:application.properties")
 public class App 
 {
     public static void main( String[] args )
             
     {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         MovieController movieController =  context.getBean(MovieController.class);
 
 
