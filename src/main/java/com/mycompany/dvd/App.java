16b80878bda579d93a9e5dd519215685b014eef8
PeterFlandin <peterflandin@gmail.com>
2023-06-20 11:39:01 +0200

    ajout de spring

--- src/main/java/com/mycompany/dvd/App.java
+++ src/main/java/com/mycompany/dvd/App.java
@@ -1,20 +1,21 @@
 package com.mycompany.dvd;
 
 import com.mycompany.dvd.controller.MovieController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 
-@Configuration
-@ComponentScan(basePackages = {"com.mycompany.dvd.controller", "com.mycompany.dvd.service", "com.mycompany.dvd.repository.file"})
-@ImportResource("classpath:applicationContext.xml")
-@PropertySource("classpath:application.properties")
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
             
     {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        //ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class, args);
+
         MovieController movieController =  context.getBean(MovieController.class);
 
 
